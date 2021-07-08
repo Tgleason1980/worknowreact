6cfa983fec24ddbb8e090e6a5a3311ab4c42f9ec
Tgleason1980 <terryscoding@gmail.com>
2021-07-08 14:55:57 -0500

    test commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Home from './components/Home';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Home/>
+       <Login/>
+        
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import '../index.css'
+
+
+const Home = ()=>{
+    return (
+        <div className='main_container'>
+            <h1 id='title'>Work Now</h1>
+            <div id='nav'>
+                <a>Home</a>
+                <a>Contact</a>
+                <a>Sign Up</a>
+                <a>Log In</a>
+            </div>
+            <hr/>
+        </div>
+    )
+}
+
+export default Home;
